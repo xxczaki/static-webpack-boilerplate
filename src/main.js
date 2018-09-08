931dd4d5a03b3a65b9745c25644dca943c53dc44
Antoni Kepinski <keppi@o2.pl>
2018-09-08 17:21:43 +0200

    ✨

---
+++ src/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// Devtools console message ✨
+console.log('%cHello World!', 'color: black; font-size: 25px; font-weight: bold;');
---
+++ webpack.config.babel.js
@@ -0,0 +1,77 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ResourceHintWebpackPlugin from 'resource-hints-webpack-plugin';
+import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
+import SizePlugin from 'size-plugin';
+
+module.exports = {
+	entry: {
+		app: [
+			'@babel/polyfill',
+			'./src/app.js'
+		]
+	},
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'main.js'
+	},
+	stats: 'minimal',
+	devServer: {
+		compress: true,
+		stats: 'minimal'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: [
+						'@babel/preset-env'
+					]
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader'
+				]
+			},
+			{
+				test: /\.html$/,
+				use: [
+					'html-loader'
+				]
+			},
+			{
+				test: /\.(png|jpg|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'images/'
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new SizePlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new ScriptExtHtmlWebpackPlugin({
+			defaultAttribute: 'async'
+		}),
+		new ResourceHintWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'main.css'
+		})
+	]
+};
